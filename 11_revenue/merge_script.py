ab4d6b8393a131ff9815d90f475cdddf20003d55
noahnoahnoahtran <tranthaingochuong@gmail.com>
2025-09-24 01:46:52 +0700

    update script

--- 11_revenue/merge_script.py
+++ 11_revenue/merge_script.py
@@ -15,12 +15,8 @@ all_data = []
 for file in excel_files:
     filename = os.path.basename(file).replace(".xlsx", "")
     employee_id = filename.split("_")[0] if "_" in filename else None
-
-
-    
-    parts = filename.split("_")
-    eng_name = parts[1] if len(parts) > 1 else None
-    vie_name = parts[2] if len(parts) > 2 else None
+    employee_eng_name = filename.split("_")[1] if "_" in filename else None
+    employee_vie_name = filename.split("_")[2] if "_" in filename else None
 
     xls = pd.ExcelFile(file)
     for sheet_name in xls.sheet_names:
@@ -32,24 +28,42 @@ for file in excel_files:
             usecols="A:S"
             )
         
+# Remove empty rows
         df = df[df.iloc[:, 1].notna()]
-        
         df =df.dropna(how="all")
 
         if not df.empty:
             df["employee_id"] = employee_id
-            df["employee_eng_name"] = eng_name
-            df["employee_vie_name"] = vie_name
-            df["employee_position"] = position
-            df["employee team"] = team
+            df["employee_eng_name"] = employee_eng_name
+            df["employee_vie_name"] = employee_vie_name
             df["source_file"] = filename
             df["sheet_name"] = sheet_name
             all_data.append(df)
 
-if all_data:
-    raw_df = pd.concat(all_data, ignore_index=True)
-    
-    raw_df.to_csv(output_path, index=False)
-    print("CSV created successfully.")
-else:
-    print("No data found.")
+# Merge all data into raw_df
+if not all_data:
+    raise ValueError("No data in folder.")
+
+raw_df = pd.concat(all_data, ignore_index=True)
+
+# Standardize data type of Payment Date column into datetime
+raw_df["Payment Date"] = pd.to_datetime(raw_df["Payment Date"], errors="coerce")
+
+# Read employee_tenure file
+emp_tenure = pd.read_csv(employee_tenure, parse_dates=["start_date", "end_date"], dayfirst=True)
+
+# Merge by employee_id
+merged = raw_df.merge(emp_tenure[["employee_id", "start_date", "end_date", "employee_team", "employee_position"]], on="employee_id", how="left")
+
+# Set up lookup conditions
+mask = (merged["Payment Date"] >= merged["start_date"]) & (merged["Payment Date"] <= merged["end_date"])
+
+# Fill team/position columns only when lookup conditions are satisfied, if not satisfied, then write "None"
+merged.loc[~mask, ["employee_team", "employee_position"]] = None
+
+# Remove unnecessary tenure columns
+merged = merged.drop(columns=["start_date", "end_date"])
+
+# Export csv file
+merged.to_csv(output_path, index=False)
+print("CSV created successfully.")
